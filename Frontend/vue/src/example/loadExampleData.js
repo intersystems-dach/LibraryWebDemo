5d2ffefaf1c39fd8b20278245a8b27cef4609cd2
phil1436 <philxsb@gmail.com>
2023-12-21 16:58:56 +0100

    added demo data

---
+++ Frontend/vue/src/example/loadExampleData.js
@@ -0,0 +1,68 @@
+const exampleBooks = [
+  { book: "Death of a Salesman", author: "Arthur Miller", genre: 2 },
+  { book: "The Importance of Being Earnest", author: "Oscar Wilde", genre: 1 },
+  { book: "Catch-22", author: "Joseph Heller", genre: 1 },
+  { book: "The Fault in Our Stars", author: "John Green", genre: 2 },
+  { book: "Good Omens", author: "Neil Gaiman, Terry Pratchett", genre: 1 },
+  { book: "A Confederacy of Dunces", author: "John Kennedy Toole", genre: 1 },
+  { book: "Macbeth", author: "William Shakespeare", genre: 2 },
+  { book: "Three Men in a Boat", author: "Jerome K. Jerome", genre: 1 },
+  { book: "Romeo and Juliet", author: "William Shakespeare", genre: 2 },
+  { book: "Othello", author: "William Shakespeare", genre: 2 },
+];
+
+const examplePersons = [
+  { friendName: "John Doe", friendNumber: "555-1234" },
+  { friendName: "Jane Smith", friendNumber: "555-5678" },
+  { friendName: "Bob Johnson", friendNumber: "555-9876" },
+  { friendName: "Alice Williams", friendNumber: "555-4321" },
+  { friendName: "Charlie Brown", friendNumber: "555-8765" },
+  { friendName: "Eva Davis", friendNumber: "555-2109" },
+  { friendName: "Frank Miller", friendNumber: "555-6543" },
+  { friendName: "Grace Taylor", friendNumber: "555-1098" },
+  { friendName: "Harry Wilson", friendNumber: "555-3456" },
+  { friendName: "Ivy Anderson", friendNumber: "555-7890" },
+];
+
+export const loadExampleBooks = () => {
+  const baseUrl = `${process.env.VUE_APP_IRIS_API_URL}/LibraryApi/addbook`;
+  exampleBooks.forEach((book) => {
+    fetch(
+      baseUrl + `?title=${book.book}&author=${book.author}&genre=${book.genre}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  });
+};
+
+export const loadExamplePersons = () => {
+  const baseUrl = `${process.env.VUE_APP_IRIS_API_URL}/LibraryApi/addfriend`;
+  examplePersons.forEach((person) => {
+    axios;
+    fetch(baseUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  });
+};
--- Frontend/vue/src/pages/dashboard/DashboardPage.vue
+++ Frontend/vue/src/pages/dashboard/DashboardPage.vue
@@ -5,12 +5,12 @@
         <v-card
           class="h-full"
           style="min-height: 380px"
-          img="https://t4.ftcdn.net/jpg/03/32/82/91/360_F_332829173_V4kiRCM2s24t1h5xs2mamqchsQmFLedg.jpg">
+          img="https://t4.ftcdn.net/jpg/03/32/82/91/360_F_332829173_V4kiRCM2s24t1h5xs2mamqchsQmFLedg.jpg"
+        >
         </v-card>
-        
       </v-col>
 
-     <!--  <v-col cols="12" md="6" xl="4">
+      <!--  <v-col cols="12" md="6" xl="4">
         <bar></bar>
       </v-col>
 
@@ -19,46 +19,46 @@
       </v-col> -->
     </v-row>
 
-   
-
-    
     <v-row class="flex-grow-0" dense>
-      
       <v-col cols="12" xl="12">
-        <books-card
-          class="h-full"
-          color="#8c9eff"
-          
-        ></books-card>
+        <books-card class="h-full" color="#8c9eff"></books-card>
       </v-col>
     </v-row>
-    
+
     <v-row class="flex-grow-0" dense>
-      
       <v-col cols="12" xl="12">
-        <people-card
-          class="h-full"
-          color="#8c9eff"
-          
-        ></people-card>
+        <people-card class="h-full" color="#8c9eff"></people-card>
       </v-col>
+    </v-row>
+    <v-row class="flex-grow-0 justify-center" dense>
+      <p></p>
+      <v-btn class="ma-2 primary" elevation="21" @click="loadDemoBooks()">
+        Add Demo Books
+      </v-btn>
+      <v-btn class="ma-2 primary" elevation="21" @click="loadDemoPeople()">
+        Add Demo Persons
+      </v-btn>
     </v-row>
   </div>
 </template>
 
 <script>
 // DEMO Cards for dashboard
-import ActivityCard from '../../components/dashboard/ActivityCard'
-import SalesCard from '../../components/dashboard/SalesCard'
-import TrackCard from '../../components/dashboard/TrackCard'
-import TableCard from '../../components/dashboard/TableCard'
-import SourcesCard from '../../components/dashboard/SourcesCard'
-import TicketsCard from '../../components/dashboard/TicketsCard'
-import TodoCard from '../../components/dashboard/TodoCard'
-import BooksCard from '../../components/dashboard/BooksCard.vue'
-import PeopleCard from '../../components/dashboard/PeopleCard.vue'
-import bar from '../ui/charts/_examples/apex-charts/simple/bar.vue'
-import radial from '../ui/charts/_examples/apex-charts/simple/radial.vue'
+import ActivityCard from "../../components/dashboard/ActivityCard";
+import SalesCard from "../../components/dashboard/SalesCard";
+import TrackCard from "../../components/dashboard/TrackCard";
+import TableCard from "../../components/dashboard/TableCard";
+import SourcesCard from "../../components/dashboard/SourcesCard";
+import TicketsCard from "../../components/dashboard/TicketsCard";
+import TodoCard from "../../components/dashboard/TodoCard";
+import BooksCard from "../../components/dashboard/BooksCard.vue";
+import PeopleCard from "../../components/dashboard/PeopleCard.vue";
+import bar from "../ui/charts/_examples/apex-charts/simple/bar.vue";
+import radial from "../ui/charts/_examples/apex-charts/simple/radial.vue";
+import {
+  loadExampleBooks,
+  loadExamplePersons,
+} from "../../example/loadExampleData";
 export default {
   components: {
     SalesCard,
@@ -71,7 +71,7 @@ export default {
     BooksCard,
     PeopleCard,
     bar,
-    radial
+    radial,
   },
   data() {
     return {
@@ -82,50 +82,65 @@ export default {
       isLoading3: true,
       isLoading4: true,
 
-      ordersSeries: [{
-        name: 'Orders',
-        data: [
-          ['2020-02-02', 34],
-          ['2020-02-03', 43],
-          ['2020-02-04', 40],
-          ['2020-02-05', 43]
-        ]
-      }],
+      ordersSeries: [
+        {
+          name: "Orders",
+          data: [
+            ["2020-02-02", 34],
+            ["2020-02-03", 43],
+            ["2020-02-04", 40],
+            ["2020-02-05", 43],
+          ],
+        },
+      ],
 
-      customersSeries: [{
-        name: 'Customers',
-        data: [
-          ['2020-02-02', 13],
-          ['2020-02-03', 11],
-          ['2020-02-04', 13],
-          ['2020-02-05', 12]
-        ]
-      }]
-    }
+      customersSeries: [
+        {
+          name: "Customers",
+          data: [
+            ["2020-02-02", 13],
+            ["2020-02-03", 11],
+            ["2020-02-04", 13],
+            ["2020-02-05", 12],
+          ],
+        },
+      ],
+    };
   },
   computed: {
     theme() {
       return this.$vuetify.theme.isDark
         ? this.$vuetify.theme.defaults.dark
-        : this.$vuetify.theme.defaults.light
-    }
+        : this.$vuetify.theme.defaults.light;
+    },
   },
   mounted() {
-    let count = 0
+    let count = 0;
 
     // DEMO delay for loading graphics
     this.loadingInterval = setInterval(() => {
-      this[`isLoading${count++}`] = false
-      if (count === 4) this.clear()
-    }, 400)
+      this[`isLoading${count++}`] = false;
+      if (count === 4) this.clear();
+    }, 400);
   },
   beforeDestroy() {
-    this.clear()
+    this.clear();
   },
   methods: {
     clear() {
-      clearInterval(this.loadingInterval)
-    }
-  }
-}
+      clearInterval(this.loadingInterval);
+    },
+    loadDemoBooks() {
+      loadExampleBooks();
+      // reload the books
+      window.location.reload();
+    },
+
+    loadDemoPeople() {
+      loadExamplePersons();
+      // reload the books
+      window.location.reload();
+    },
+  },
+};
 </script>
